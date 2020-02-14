7c379101ac964479d1b785e5360243f972edada1
PickleGawd <42813970+PickleGawd@users.noreply.github.com>
2020-02-14 10:47:08 -0800

    First commit

---
+++ src/pages/home.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+
+import Bounty from "../components/Bounty";
+import Profile from "../components/Profile";
+
+export class home extends Component {
+  state = {
+    bounties: null
+  };
+
+  componentDidMount() {
+    axios
+      .get("/bounties")
+      .then(res => {
+        this.setState({ bounties: res.data });
+      })
+      .catch(err => console.log(err));
+  }
+  render() {
+    let recentBountiesMarkup = this.state.bounties ? (
+      this.state.bounties.map(bounty => (
+        <Bounty key={bounty.bountyId} bounty={bounty} />
+      ))
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <Grid container spacing={10}>
+        <Grid item sm={8} xs={12}>
+          {recentBountiesMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <Profile></Profile>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default home;

0fb112fb6221f3de4e8a1cf7c02a3d892500d592
ThePickleGawd <42813970+PickleGawd@users.noreply.github.com>
2020-04-24 10:41:06 -0700

    fixed redux-dev-tools error

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
-import withStyles from '@material-ui/core/styles/withStyles';
-import PropTypes from 'prop-types';
-import AppIcon from '../images/favicon.ico';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from "prop-types";
+import AppIcon from "../images/favicon.ico";
+import { Link } from "react-router-dom";
 
 //MUI Stuff
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 // Redux stuff
-import { connect } from 'react-redux';
-import { loginUser } from '../redux/actions/userActions';
+import { connect } from "react-redux";
+import { loginUser } from "../redux/actions/userActions";
 
-const styles = theme => ({
-  ...theme.spreadIt
+const styles = (theme) => ({
+  ...theme.spreadIt,
 });
 
 class login extends Component {
   constructor() {
     super();
     this.state = {
-      email: '',
-      password: '',
-      errors: {}
+      email: "",
+      password: "",
+      errors: {},
     };
   }
   componentWillReceiveProps(nextProps) {
@@ -33,40 +33,40 @@ class login extends Component {
       this.setState({ errors: nextProps.UI.errors });
     }
   }
-  handleSubmit = event => {
+  handleSubmit = (event) => {
     event.preventDefault();
 
     const userData = {
       email: this.state.email,
-      password: this.state.password
+      password: this.state.password,
     };
     this.props.loginUser(userData, this.props.history);
   };
-  handleChange = event => {
+  handleChange = (event) => {
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
   render() {
     const {
       classes,
-      UI: { loading }
+      UI: { loading },
     } = this.props;
     const { errors } = this.state;
     return (
       <Grid container className={classes.form}>
         <Grid item sm />
         <Grid item sm>
-          <img src={AppIcon} alt='hwbounty logo' className={classes.image} />
-          <Typography variant='h2' className={classes.pageTitle}>
+          <img src={AppIcon} alt="hwbounty logo" className={classes.image} />
+          <Typography variant="h2" className={classes.pageTitle}>
             Login
           </Typography>
           <form noValidate onSubmit={this.handleSubmit}>
             <TextField
-              id='email'
-              name='email'
-              type='email'
-              label='Email'
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
               className={classes.textField}
               helperText={errors.email}
               error={errors.email ? true : false}
@@ -75,10 +75,10 @@ class login extends Component {
               fullWidth
             />
             <TextField
-              id='password'
-              name='password'
-              type='password'
-              label='Password'
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
               className={classes.textField}
               helperText={errors.password}
               error={errors.password ? true : false}
@@ -87,14 +87,14 @@ class login extends Component {
               fullWidth
             />
             {errors.general && (
-              <Typography variant='body2' className={classes.customError}>
+              <Typography variant="body2" className={classes.customError}>
                 {errors.general}
               </Typography>
             )}
             <Button
-              type='submit'
-              variant='contained'
-              color='primary'
+              type="submit"
+              variant="contained"
+              color="primary"
               className={classes.button}
               disabled={loading}
             >
@@ -105,7 +105,7 @@ class login extends Component {
             </Button>
             <br />
             <small>
-              Don't have an account ? sign up <Link to='/signup'>here</Link>
+              Don't have an account ? sign up <Link to="/signup">here</Link>
             </small>
           </form>
         </Grid>
@@ -119,16 +119,16 @@ login.propTypes = {
   classes: PropTypes.object.isRequired,
   loginUser: PropTypes.func.isRequired,
   user: PropTypes.object.isRequired,
-  UI: PropTypes.object.isRequired
+  UI: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   user: state.user,
-  UI: state.UI
+  UI: state.UI,
 });
 
 const mapActionsToProps = {
-  loginUser
+  loginUser,
 };
 
 export default connect(
